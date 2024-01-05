7d10cb31e8e3786e25df4fbb8a0ece059a09a382
woodyvalentino123 <69005803+woodyvalentino123@users.noreply.github.com>
2024-01-05 16:45:55 +0530

    Add files via upload

---
+++ PRO-C214-StudentBoilerplate-main/script.js
@@ -0,0 +1,14 @@
+$(function(){
+    $("#show_chat").click(function(){
+        $(".left-window").css("display","none")
+        $(".right-window").css("display","block")
+        $(".header_back").css("display","block")
+      
+
+    })
+    $(".header_back").click(function(){
+        $(".left-window").css("display","block")
+        $(".right-window").css("display","none")
+        $(".header_back").css("display","none")
+    })
+})
